2e8fba2ded528baf600d409dafbf0a4d9ad946c4
taverasmisael <macrot29@gmail.com>
2019-01-25 11:24:59 -0400

    core: configure all linterns and test envs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {ReactComponent as Logo} from './logo.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <Logo />
+          <p>Welcome to React</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,8 +18,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
